630e837b74b0c6ff5c1e42ca633eaa0689515fd1
Irfana Syed <110710568+irfana2003@users.noreply.github.com>
2025-05-27 11:24:39 +0000

    added coverage and uploaded it
    Signed-off-by: Irfana Syed <110710568+irfana2003@users.noreply.github.com>

--- cal_test.go
+++ cal_test.go
@@ -12,3 +12,24 @@ func TestAdd(t *testing.T){
 	assert.Equal(result, 3)
 	assert.NoError(err)
 }
+
+func TestSub(t *testing.T){
+	assert := assert.New(t)
+	result, err := Sub(1,2)
+	assert.Equal(result, -1)
+	assert.NoError(err)
+}
+
+func TestMul(t *testing.T){
+	assert := assert.New(t)
+	result, err := Mul(1,2)
+	assert.Equal(result, 2)
+	assert.NoError(err)
+}
+
+func TestDiv(t *testing.T){
+	assert := assert.New(t)
+	result, err := Div(4,2)
+	assert.Equal(result, 2)
+	assert.NoError(err)
+}
